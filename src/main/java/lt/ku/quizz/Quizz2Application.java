82e5d5c19768c1387fd66a6203a28c36ff3df5ac
KarolinaRi <k.ripinskyte@gmail.com>
2023-01-07 23:14:52 +0200

    Update Quizz2Application.java

--- src/main/java/lt/ku/quizz/Quizz2Application.java
+++ src/main/java/lt/ku/quizz/Quizz2Application.java
@@ -2,6 +2,7 @@ package lt.ku.quizz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Quizz2Application {
@@ -9,5 +10,10 @@ public class Quizz2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Quizz2Application.class, args);
 	}
+	
+	 @RequestMapping("/")
+	    String index() {
+	        return "home";
+	    }
 
 }
